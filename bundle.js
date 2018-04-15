39fb017d6a60d471d415ae95bbd0777f72c918a6
tyw66 <2639144635@qq.com>
2018-04-15 21:07:29 +0800

    little revise

--- bundle.js
+++ bundle.js
@@ -7,7 +7,7 @@ function init(){
 	var time=date.getTime()-setupDate.getTime();
 	//计算出相差天数
 	var days=Math.floor(time/(24*3600*1000))
-	document.getElementById('dateNav').innerHTML="当前："+date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDay()+"日";
+	document.getElementById('dateNav').innerHTML="当前："+date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日";
 	document.getElementById('timeSpan').innerHTML="网站已建立"+days+"天";
 }
 //每秒执行一次
