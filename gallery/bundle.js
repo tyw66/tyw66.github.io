fe41d5ac1c33ee3b77a1943ec11f3f9edf8f10af
tyw66 <2639144635@qq.com>
2018-05-02 21:57:15 +0800

    根据《jsDOM编程艺术》优化下

--- gallery/bundle.js
+++ gallery/bundle.js
@@ -1,15 +1,21 @@
 function showPic(whichpic){
-	var source = whichpic.getAttribute("href");
-	var imagezone = document.getElementById("imagezone");
-	imagezone.setAttribute("src",source);
+	var imgZone = document.getElementById("imgZone");
+	if(!imgZone) return false;
 	
-	var words = whichpic.getAttribute("title");
+	var source = whichpic.getAttribute("href");
+	imgZone.setAttribute("src",source);
+	
 	var description = document.getElementById("description");
+	if(description){
+		//检测
+		var words = whichpic.getAttribute("title")?whichpic.getAttribute("title"):"";
+		//只有1个内容时，等价的
+		description.childNodes[0].nodeValue=words;
+		//description.firstChild.nodeValue=words;
+		//description.innerHTML=words;		
+	}
 
-	////只有1个内容时，等价的
-	description.childNodes[0].nodeValue=words;
-	description.firstChild.nodeValue=words;
-	//description.innerHTML=words;
+	return true;
 }
 
 function countbodychild(){
@@ -17,4 +23,33 @@ function countbodychild(){
 	alert(body_elem.childNodes.length);
 }
 
-//window.onload = countbodychild;
+
+function initGallery(){
+	//检测
+	if(!document.getElementById || !document.getElementsByClassName){
+		return false;
+	}
+	
+	var gallery = document.getElementById("imgGallery");
+	if(!gallery){
+		return false;
+	}
+	
+	//遍历链接，添加点击事件
+	var links = gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+		links[i].onclick = function(){
+			return !showPic(this);
+			//return !showPic(links[i]);//todo: 需用this, 这样写就不行
+		}	
+
+		
+	}
+}	
+
+
+window.onload = function(){
+	initGallery();
+	//alert(typeof initGallery);
+	//countbodychild();
+}
