921d455ba4aa46d41228c186f87d30b3474a52e9
tyw66 <2639144635@qq.com>
2022-02-22 00:19:27 +0800

    中国传统色

--- bundle.js
+++ bundle.js
@@ -9,6 +9,10 @@ function init(){
 	var days=Math.floor(time/(24*3600*1000))
 	document.getElementById('dateNav').innerHTML="当前："+date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日";
 	document.getElementById('timeSpan').innerHTML="网站已建立"+days+"天";
+
+	//获取当前年份，显示版权信息
+	var copyRightInfo="Copyright &copy 2017 - "+date.getFullYear()+" tyw66. All Rights Reserved. tyw66@qq.com 版权所有";
+	document.getElementById('copyRight').innerHTML=copyRightInfo;
 }
 //每秒执行一次
 //setInterval("window.onload()","1000");		
