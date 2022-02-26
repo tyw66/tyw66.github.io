5a148dbfeecd785cf0ed2e8b9b3c3fdddb711b4f
tyw66 <2639144635@qq.com>
2022-02-26 21:59:12 +0800

    中国传统色-CSS排版

--- 11chinese_color/chinese_color.js
+++ 11chinese_color/chinese_color.js
@@ -24,6 +24,7 @@ function diaplay(jsonData) {
         termDiv.className = 'termArea';
         mainArea.append(termDiv);
 
+        //节气名称
         var termHeader = document.createElement("h3");
         termDiv.append(termHeader);
         termHeader.innerHTML = `${term}`;
@@ -31,11 +32,15 @@ function diaplay(jsonData) {
         termHeader.className = 'term-name';
         var weatherData = jsonData[term];
 
-        //三侯
+        //本节气+三侯
         for (weather in weatherData) {
+            //
+            var weatherDiv = document.createElement("div");
+            termDiv.append(weatherDiv);
+            weatherDiv.className = "weatherArea";
 
-            var weatherP = document.createElement("h4");
-            termDiv.append(weatherP);
+            var weatherP = document.createElement("span");
+            weatherDiv.append(weatherP);
             weatherP.innerHTML = `${weather}`;
             weatherP.className = "weather-name";
             var colorData = weatherData[weather];
@@ -47,21 +52,21 @@ function diaplay(jsonData) {
                 var g = parseInt(rgbData[1]);
                 var b = parseInt(rgbData[2]);
 
-                var txt = `${colorData[i].NAME}: R${r} G${g} B${b}`
+                var txt = `< ${colorData[i].NAME} ><br/>R${r} G${g} B${b}`
                 var rgbHex = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 
-                var li = document.createElement("span");
-                termDiv.append(li);
-                li.innerHTML = txt;
-                li.style.backgroundColor = rgbHex;
+                var colorPiece = document.createElement("span");
+                weatherDiv.append(colorPiece);
+                colorPiece.innerHTML = txt;
+                colorPiece.style.backgroundColor = rgbHex;
                 if (r + g + b < 383) {
-                    li.style.color = "#FFF";
+                    colorPiece.style.color = "#FFF";
                 }
-                li.className = "color-name";
+                colorPiece.className = "color-name";
             }
 
-            var br = document.createElement("br");
-            termDiv.append(br);
+            // var br = document.createElement("br");
+            // termDiv.append(br);
         }
 
 
